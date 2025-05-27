3e67a6d9074d3d99cefef1b85c5869a4b0d03f57
AndriuuU <andresitomolinaclan@gmail.com>
2025-05-27 16:11:54 +0200

    Emoticonos

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
   <div class="app-container">
     <app-header />
     <main class="app-content">
--- src/main.js
+++ src/main.js
@@ -17,11 +17,11 @@ if (savedTheme) {
 
 const pinia = createPinia()
 const app = createApp(App)
-app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(pinia)
 app.use(router)
 
 const authStore = useAuthStore()
 authStore.init()
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
